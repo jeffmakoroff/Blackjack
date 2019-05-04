7ec77784ba3b8d2b445c9cc85cf1b8b713f1ec0a
Jeff Makoroff <mako-mac@Jeffs-MacBook-Pro.local>
2019-05-03 22:03:28 -0500

    first commit

---
+++ blackJack.js
@@ -0,0 +1,37 @@
+//Blackjack
+// by Jeff Makoroff
+
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten',
+    'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three',
+    'Two'];
+
+
+
+function createDeck() {
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        }
+    }
+    return deck;
+}
+
+function getNextCard(){
+    return deck.shift(); //this removes the first card from index 0 to give to playerCards
+}
+
+let deck = createDeck();  //instantiates deck with the return value of the createDeck function
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+
+let playerCards = [ getNextCard(), getNextCard() ];
+
+console.log("Welcome to Blackjack!")
+
+console.log("You are dealt: ")
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
